eb882128649bfc24b93f6eeea00c011f389c16c9
LiorUbuntu <liorshahverdi@gmail.com>
2014-11-14 19:01:55 -0800

    2d matrix made

---
+++ tictac.py
@@ -0,0 +1,11 @@
+## My attempt at tic-tac-toe with python. (I don't know any Python)
+print 'Here it goes!'
+twod_list = []
+new = []
+for i in range (0,3):
+		for j in range(0,3):
+			new.append(0)
+		twod_list.append(new)
+		new = []
+
+print twod_list
