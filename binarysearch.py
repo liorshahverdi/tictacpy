9c90fb742a2e9f78fb1087c6b5cce22c5947e1ff
LiorUbuntu <liorshahverdi@gmail.com>
2014-11-15 12:27:24 -0800

    binsearch exercise

--- binarysearch.py
+++ binarysearch.py
@@ -1,11 +1,21 @@
-print 'Think of a number from 1 to 100.'
+print 'Think of a number from 0 to 100.'
 
 reply = 0
+low = 0
+high = 101
 while reply != 3:
-	low = 1
-	high = 100
 	res = int((low + high)/2)
 	print 'My guess is ', res
-	reply = int(raw_input("Higher? Press 1 Lower? Press 2 Correct? Holy shit! press 3: "))
+	print 'Higher? Press 1 Lower? Press 2 Correct? Holy shit! press 3: '
+	reply = int(raw_input())
 	while reply <= 0 or reply >=4:
 		reply = int(raw_input("Not valid try again: "))
+
+	if reply == 1:
+		low = res
+		print 'HIGHER'
+	elif reply == 2:
+		high = res
+		print 'LOWER'
+	elif reply == 3:
+		print 'COOORECT!'
