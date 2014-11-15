d262c67468d48a83573772dc96d1bf0f0448522d
LiorUbuntu <liorshahverdi@gmail.com>
2014-11-14 20:37:55 -0800

    learning

--- tictac.py
+++ tictac.py
@@ -1,11 +1,12 @@
 ## My attempt at tic-tac-toe with python. (I don't know any Python)
-print 'Here it goes!'
-twod_list = []
-new = []
-for i in range (0,3):
-	for j in range(0,3):
-		new.append(0)
-	twod_list.append(new)
-	new = []
+print "Let's play Tic Tac Toe!"
 
-print twod_list
+class Grid:
+	def __init__(self, tdList=0):
+		self.tdList = [[0,0,0], [0,0,0], [0,0,0]]
+
+	def dispList(self):
+		print self.tdList
+
+g1 = Grid()
+g1.dispList()
